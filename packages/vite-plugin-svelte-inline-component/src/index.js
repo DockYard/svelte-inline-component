3bc35cea06971d9a59361730bb4bba7f95b29a69
Miguel Camba <miguel.camba@gmail.com>
2022-01-11 00:56:30 +0100

    Save small change

--- packages/vite-plugin-svelte-inline-component/src/index.js
+++ packages/vite-plugin-svelte-inline-component/src/index.js
@@ -1,19 +1,16 @@
 export default function vitePluginSvelteInlineComponents() {
-  const virtualModulePrefix = 'virtual:inline-svelte'
-  const resolvedVirtualModulePrefix = '\0' + virtualModulePrefix
-
   return {
     name: 'svelte-inline-component',
     enforce: 'pre',
 
     resolveId(id) {
       if (id.startsWith('virtual:inline-svelte')) {
-        return '\0' + id;
+        return id;
       }
     },
 
     load(id) {
-      if (id.startsWith(resolvedVirtualModulePrefix)) {
+      if (id.startsWith('virtual:inline-svelte')) {
         let base64ComponentSource = id.split(/virtual:inline-svelte:/)[1].slice(0, -7);
         let buf = Buffer.from(base64ComponentSource, 'base64');
         return buf.toString('utf8');
